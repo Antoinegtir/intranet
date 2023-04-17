0c91e773809cb2420aeebba63d8dfdb98507ad21
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-04-18 01:08:57 +0200

    :art: add theme of intra

---
+++ lib/theme/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// [IntraColor], all colors used in the app
+/// [epiblue], [Color] color of epitech logo
+/// [white], [Color] color of text
+/// [black], [Color] color of background
+
+class IntraColor {
+  static const Color epiblue = Color(0xff25599d);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
---
+++ lib/theme/text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:intra/theme/color.dart';
+
+/// [textStyle], [TextStyle] style of text
+/// [fontFamily], [String] font of text
+/// [color], [Color] color of text
+
+TextStyle textStyle = const TextStyle(
+    fontFamily: "icons.ttf",
+    color: IntraColor.black,
+    fontSize: 25,
+    fontWeight: FontWeight.w900);
